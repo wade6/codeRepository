4f5e4cf8982dfec8f6386f023db3a988d7e262f4
wade6 <wade6hm@gmail.com>
2018-04-09 00:25:50 +0800

    add

--- src/main/java/com/code/repository/Application.java
+++ src/main/java/com/code/repository/Application.java
@@ -1,12 +1,7 @@
 package com.code.repository;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class Application {
--- src/main/java/com/code/repository/controller/HelloController.java
+++ src/main/java/com/code/repository/controller/HelloController.java
@@ -1,13 +1,16 @@
-package com.code.repository.hello;
+package com.code.repository.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //  combines @Controller and @ResponseBody
 public class HelloController {
+	
+	private String name;
 
 	@RequestMapping("/")
 	public String index() {
-		return "Hello world!";
+		return "Hello "+name;
 	}
 }
