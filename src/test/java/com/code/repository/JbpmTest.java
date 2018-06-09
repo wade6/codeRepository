37136cd55e2ad0c00ed192d02981ec045c7987f7
wade6 <wade6hm@gmail.com>
2018-06-09 19:34:22 +0800

    jbpm流程引擎基本流程跑通

--- src/test/java/com/code/repository/JbpmTest.java
+++ src/test/java/com/code/repository/JbpmTest.java
@@ -1,14 +1,12 @@
 package com.code.repository;
-import org.jbpm.api.Configuration;
-import org.jbpm.api.ProcessEngine;
-import org.jbpm.api.RepositoryService;
+import org.jbpm.api.*;
 import org.jbpm.api.task.Task;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
+import java.util.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,26 +22,35 @@ public class JbpmTest {
         new org.hibernate.cfg.Configuration().configure("jbpm.hibernate.cfg.xml").buildSessionFactory();
     }
 
-    // 一、部署流程定义
+    // 一、部署流程定义 RepositoryService
     @Test
-    public void deployProcessDefinition() {
-        ProcessEngine processEngine = Configuration.getProcessEngine();
-        RepositoryService repositoryService = processEngine.getRepositoryService();
-        repositoryService.createDeployment().addResourceFromClasspath("jbpm.jpdl.xml").deploy();
+    public void deploy() {
+        ProcessEngine processEngine = Configuration.getProcessEngine();// 创建流程引擎
+        RepositoryService repositoryService = processEngine.getRepositoryService();// 获取流程资源服务接口
+        repositoryService.createDeployment().addResourceFromClasspath("test.jpdl.xml").deploy();// 部署流程
     }
 
+    // 生成一个流程实例   ExecutionService
     @Test
-    public void startProcessInstance() {
+    public void createInstance() {
         ProcessEngine processEngine = Configuration.getProcessEngine();
-        processEngine.getExecutionService().startProcessInstanceByKey("test");
+        String processKey = "test";
+        ExecutionService executionService = processEngine.getExecutionService();//执行服务
+        Map<String, String> map = new HashMap<String, String>(); // 添加变量
+        map.put("staff","hm员工");
+        ProcessInstance processInstance = executionService.startProcessInstanceByKey(processKey,map);
+        String id = processInstance.getId();
+        System.out.println("流程实例id："+id);
     }
 
+    // 查询任务列表 TaskService
     @Test
     public void findMyTaskList() {
         ProcessEngine processEngine = Configuration.getProcessEngine();
         // 查询任务
         String userId = "员工";
-        List<Task> list = processEngine.getTaskService().findPersonalTasks(userId);
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> list = taskService.findPersonalTasks(userId);
 
         // 显示任务
         System.out.println("========= 【"+userId+"】的未办理的任务列表 =========");
@@ -55,13 +62,32 @@ public class JbpmTest {
         }
     }
 
+    // 获取流程变量  TaskService
+    @Test
+    public void fetchTaskVariable() {
+        ProcessEngine processEngine = Configuration.getProcessEngine();
+        String taskId = "40003";
+        String instanceId= "test.40001";
+        TaskService taskService = processEngine.getTaskService();
+        ExecutionService executionService = processEngine.getExecutionService();
+        // 取出当前的所有任务
+        Set<String> activityNames = executionService.createProcessInstanceQuery().processInstanceId(instanceId).uniqueResult().findActiveActivityNames();
+        String name = (String)activityNames.toArray()[0];
+        Task task = taskService.createTaskQuery().processInstanceId(instanceId).activityName(name).uniqueResult();
+        // 根据任务取出变量
+        Set<String> vars =  taskService.getVariableNames(task.getId());
+        System.out.println(vars.toString());
+        String staff = (String)taskService.getVariable(taskId,"staff");
+        System.out.println("staff:"+staff);
+    }
+
+    // 完成当前任务  TaskService
     @Test
     public void completeTask() {
         ProcessEngine processEngine = Configuration.getProcessEngine();
-        String taskId = "20002";
-        processEngine.getTaskService().completeTask(taskId);
+        String taskId = "60001";
+        TaskService taskService = processEngine.getTaskService();
+        taskService.completeTask(taskId);
     }
-
-
 
 }
